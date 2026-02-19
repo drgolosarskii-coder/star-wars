2b766a96343f66e422277a876ddf30519684a331
drgolosarskii-coder <dr.golosarskii@gmail.com>
2026-02-19 19:18:20 +0200

    Refactor OpeningCrawl component to fetch data dynamically and replace static constants with API integration

--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -1,29 +1,35 @@
-import {useEffect, useState} from "react";
-import {base_url} from "../utils/constants.js";
+import { useEffect, useState } from "react";
+import { base_url } from "../utils/constants.js";
 
 const OpeningCrawl = () => {
-    const [openingCrawl, setOpeningCrawl] = useState()
+    const [openingCrawl, setOpeningCrawl] = useState(() => {
+        return sessionStorage.getItem("opening_crawl");
+    });
 
     useEffect(() => {
-        const episode = Math.floor(Math.random() * 6) + 1
-        fetch(`${base_url}/v1/films/${episode}`)
-            .then(res => res.json())
-            .then(data => setOpeningCrawl(data.opening_crawl))
-            .catch(() => setOpeningCrawl('Error loading opening crawl'))
-    }, [])
+        if (!openingCrawl) {
+            const episode = Math.floor(Math.random() * 6) + 1;
+
+            fetch(`${base_url}/v1/films/${episode}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setOpeningCrawl(data.opening_crawl);
+                    sessionStorage.setItem("opening_crawl", data.opening_crawl);
+                })
+                .catch(() => setOpeningCrawl("Error loading opening crawl"));
+        }
+    }, [openingCrawl]);
 
     if (openingCrawl) {
-        return (
-            <p className="far-galaxy">{openingCrawl}</p>
-        )
+        return <p className="far-galaxy">{openingCrawl}</p>;
     } else {
         return (
             <p className="far-galaxy">
-                <span className={'spinner-border spinner-border-sm'}></span>
-                <span className={'spinner-grow spinner-grow-sm'}>Loading...</span>
+                <span className="spinner-border spinner-border-sm"></span>
+                <span className="spinner-grow spinner-grow-sm">Loading...</span>
             </p>
-        )
+        );
     }
-}
+};
 
 export default OpeningCrawl;
